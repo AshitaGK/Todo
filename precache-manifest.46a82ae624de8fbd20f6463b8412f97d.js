e563b6a5fe040940855f0ea95be6cbec85f98b6d
AshitaGK <ashita.gk696@gmail.com>
2020-02-06 11:42:31 +0530

    Updates

---
+++ precache-manifest.46a82ae624de8fbd20f6463b8412f97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fff0a749248473da66d3ac2b1d67ed",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "82ea61ecd6140ae6b7ac",
+    "url": "/Todo/static/css/main.8074beca.chunk.css"
+  },
+  {
+    "revision": "17c85de39dc99fe0a7b0",
+    "url": "/Todo/static/js/2.555a02a1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Todo/static/js/2.555a02a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ea61ecd6140ae6b7ac",
+    "url": "/Todo/static/js/main.883dcf43.chunk.js"
+  },
+  {
+    "revision": "07819277d52bea0daeca",
+    "url": "/Todo/static/js/runtime-main.804930da.js"
+  }
+]);
